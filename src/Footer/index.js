ff63dcc55bfd8a0bee072c2ba87bcb0ba8f9f47b
oriooctopus <oliverullman@gmail.com>
2020-06-23 10:30:47 -0600

    fix react warnings

--- src/Footer/index.js
+++ src/Footer/index.js
@@ -7,7 +7,7 @@ import useBaseUrl from '@docusaurus/useBaseUrl';
 import Logo from 'img/shared/logo.svg';
 import SocialLinks from './SocialLinks';
 import universalConfig from '../universal-config';
-import {WithBackgroundImage} from 'libra-components';
+import {WithBackgroundImage} from 'libra-docusaurus';
 
 import classnames from 'classnames';
 import styles from './styles.module.css';
@@ -27,10 +27,10 @@ const Footer = () => {
               <Logo alt={logo.alt} />
             </a>
           </div>
-          {links.map(({ items, type }) => (
-            <ul className={classnames(styles.linkList, styles[type])}>
+          {links.map(({ items, type }, i) => (
+            <ul className={classnames(styles.linkList, styles[type])} key={i}>
               {items.map(({ label, to }) => (
-                <li className={styles[type]}>
+                <li className={styles[type]} key={`${label}${to}`}>
                   <a href={to}>{label}</a>
                 </li>
               ))}
